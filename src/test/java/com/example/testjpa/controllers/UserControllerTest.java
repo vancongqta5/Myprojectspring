74c6d2565142d7ce24466d99973bec7f46ac370d
vancongqta5 <vancongqta5@gmail.com>
2023-03-14 13:55:24 +0700

    update

--- src/test/java/com/example/testjpa/controllers/UserControllerTest.java
+++ src/test/java/com/example/testjpa/controllers/UserControllerTest.java
@@ -24,7 +24,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class UserControllerTest {
-
     @Mock
     private UserRepository userRepository;
     @Mock
